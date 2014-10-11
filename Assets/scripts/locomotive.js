4179628463ae5154f80bd2b2e449f6a38912cf7d
Mycorvette Garcia <Mycorvette6968@gmail.com>
2014-10-11 00:33:21 -0700

    added a character controller so he actually moves
    added character controller and a locomotive script ill add jumping and
    rotating later

---
+++ Assets/scripts/locomotive.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var speed = 5.0;
+
+function Start () {
+
+}
+
+function Update () {
+
+var x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+var z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+transform.Translate(x,0,z);
+}
