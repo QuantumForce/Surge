c8f85962f7c0dd0a14b6bfcb0cd70fa1d86dda64
Mycorvette Garcia <Mycorvette6968@gmail.com>
2014-10-11 17:47:33 -0700

    character rotates
    right when you go right same with left

--- Assets/scripts/locomotive.js
+++ Assets/scripts/locomotive.js
@@ -1,7 +1,7 @@
 ﻿#pragma strict
 
 var speed = 5.0;
-
+var rotate = 5.0;
 function Start () {
 
 }
@@ -11,4 +11,10 @@ function Update () {
 var x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 var z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 transform.Translate(x,0,z);
+if (Input.GetKey("a")){
+transform.Rotate(Vector3.down * rotate);
+}
+if (Input.GetKey("d")){
+transform.Rotate(Vector3.up * rotate);
+}
 }
