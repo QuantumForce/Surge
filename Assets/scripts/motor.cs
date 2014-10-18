b8c99d2434c445ebe712c3c231c94c7614bb2b9b
Mycorvette <Mycorvette6968@gmail.com>
2014-10-18 15:55:29 -0700

    stuff

---
+++ Assets/scripts/motor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class motor : MonoBehaviour {
+	public static motor Instance;
+	public float MoveSpeed = 10f;
+	public Vector3 MoveVector { get; set; }
+
+	void Awake()
+	{
+		Instance = this;
+	}
+	void UpdateMotor () 
+	{
+		SnapAlignCharacterWithChamera ();
+		ProcessMotion ();
+	}
+	void ProcessMotion()
+	{
+
+	}
+	void SnapAlignCharacterWithChamera()
+	{
+
+	}
+}
---
+++ Assets/scripts/moving.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moving : MonoBehaviour {
+	public static CharacterController CharacterController; 
+	public static moving Instance;
+	void Awake()
+	{
+		CharacterController = GetComponent ("CharacterController") as CharacterController;
+		Instance = this;
+	}
+	void Update() 
+	{
+		if (Camera.mainCamera == null)
+			return;
+
+		GetLocomotionInput ();
+
+	}
+	void GetLocomotionInput()
+	{
+		
+	}
+}
